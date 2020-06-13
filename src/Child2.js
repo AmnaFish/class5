6219703597e8aaa472a89cde84f1bbbe0b747d9f
AmnaFish <amnasaeed100@hotmail.com>
2020-06-14 00:39:20 +0500

    Add files via upload

---
+++ src/Child2.js
@@ -0,0 +1,16 @@
+import React, {useContext,useReducer} from "react";
+import numbReducer from './numbReducer'
+//Reducer: multi changes to a state: multi component
+//use state is for perticular component
+function Child2() {
+    let [state, dispatch]= useReducer(numbReducer,52)
+  return (
+    <div>
+      child 2 {state}
+      <button onClick={()=>{dispatch({type:'INCREMENT', val:67});}}>Incrememnt</button>
+      <button onClick={()=>{dispatch({type:'DECREMENT', val:34});}}>Decrememnt</button>
+    </div>
+  );
+}
+
+export default Child2;
---
+++ src/numbReducer.js
@@ -0,0 +1,10 @@
+//pure fuction does not depend or updates on any fuction
+const numbReducer = (state, action) => {
+    switch(action.type) {
+        case 'INCREMENT':
+            return state + action.val
+        case 'DECREMENT':
+            return state - action.val
+    }
+}
+export default numbReducer;
